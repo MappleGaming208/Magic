3d2132330e78383e867b44e7c4cb312c9375a5e9
MappleGaming208 <122428938+MappleGaming208@users.noreply.github.com>
2024-12-06 14:18:52 -0700

    Update PSLIBTEST.lua

--- Scripts/Development/PSLIBTEST.lua
+++ Scripts/Development/PSLIBTEST.lua
@@ -1,119 +1,39 @@
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MappleGaming208/UILibs/refs/heads/main/sources/PlayStationLibSrc.lua"))()
+local Link = loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostDuckyy/Ui-Librarys/main/Playstation%20Ui%20Library/Source'))()
 
-local UI = GUI:CreateWindow("Magic Hub v1","Made By MappleGaming208")
+local Win = Link:CreateWindow('Name','Yeah Info') -- :CreateWindow(Title,Info) Note: info no space
 
-local Home = UI:addPage("Info",1,true,6)
+local Tab = Win:addPage('Tab', 2, true, 6) -- :addPage(Title, Scoll Size, Visible, elementspacing)
+-- Note: Dont have update function
 
-Home:addLabel("This is a Label","Lol this funny")
-
-Home:addButton("This is a button",function()
-    game.StarterGui:SetCore("SendNotification",{
-        Title = "Clicked";
-        Text = "Lo";
-    })
-end)
-
-Home:addToggle("This is a Toggle",function(value)
-    print(value)
-    if value == false then 
-        game.StarterGui:SetCore("SendNotification",{
-            Title = "Toggle";
-            Text = "false";
-        })
-    else 
-        game.StarterGui:SetCore("SendNotification",{
-            Title = "Toggle";
-            Text = "true";
-        })
-    end
-end)
-
-Home:addSlider("This is a Slider",16,100,function(value)
-    print(value)
-end)
-
-Home:addTextBox("This is a TextBox","Um",function(value)
-    game.StarterGui:SetCore("SendNotification",{
-        Title = "Wrote";
-        Text = value;
-    })
-end)
-
-Home:addDropdown("This is a Dropdown",{"Um","Yep","Lop","GG"},1,function(value)
-    game.StarterGui:SetCore("SendNotification",{
-        Title = "Selected :";
-        Text = value;
-    }) 
-end)
-
--- Just an example of how you would actually use it i guess
-
-local LP = UI:addPage("Local",2,false,6)
 
 -- Label
+Tab:addLabel('This a Label', 'Label info') -- :addLabel(Title, Info)
 
-LP:addLabel("Local","Don't use in games with anti cheats")
-
---- Button
-
-LP:addButton("DIE",function()
-    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+-- Button
+Tab:addButton('This a button', function() -- :addButton(Title, callback)
+    print('Clicked Button')
 end)
 
--- Toggle
-
-LP:addToggle("Sprint",function(value)
-    if value == false then
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-    else
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 24
+-- Toggles
+Tab:addToggle('Toggle', function(value) -- :addToggle(Title, callback)
+    if value then -- if true then
+        print('True') -- print true
+    else -- else false
+        print('False') -- print false
     end
 end)
 
 -- Slider
-
-LP:addSlider("WalkSpeed",16,150,function(value)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+Tab:addSlider('Slider', 1,100, function(value) -- :addSlider(Title, min, max, callback)
+    print(value)
 end)
 
--- Textbox
-
-LP:addTextBox("Jump Power / 50 is default","Number here",function(value)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+-- TextBox
+Tab:addTextBox('Textbox', 'Textbox Info', function(text) -- :addTextBox(Title, Info)
+    print(text)
 end)
 
--- Dropdown 
-
-local PLIST = {}
-
-for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-    table.insert(PLIST,v.DisplayName)
-end
-
-LP:addDropdown("Teleport to Player",PLIST,4,function(value)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
-end)
-
-
--- Spam 
-
-LP:addLabel("Spam","This is just to show how it looks with more ui elements")
-
-LP:addDropdown("Teleport to Player",PLIST,4,function(value)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
-end)
-
-
-LP:addDropdown("Teleport to Player",PLIST,4,function(value)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
-end)
-
-
-LP:addDropdown("Teleport to Player",PLIST,4,function(value)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
-end)
-
-
-LP:addDropdown("Teleport to Player",PLIST,4,function(value)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+-- Dropdown
+Tab:addDropdown('This is DropDown', {'1', '2', '3', '4', '5'}, 4, function(options) -- :addDropdown(Title, {'List', 'Yeah'}, Scroll Size, callback)
+    print(options)
 end)
